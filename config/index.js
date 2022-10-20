32020afc2f8898be917c669bc78e96733fc761c1
unknown <wangsy>
2022-10-20 13:35:15 +0800

    微信机器人初始化

---
+++ config/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Peanut
+ * @Description:  全局配置config
+ * @Date: 2020-05-20 22:36:09
+ * @Last Modified by:   Peanut
+ * @Last Modified time: 2020-05-20 22:36:09
+ */
+module.exports = {
+  IGNORE: ["www.iiter.cn"], //忽略某个用户的消息，填写昵称即可
+  WEBROOM: "赶紧学习，拉开距离", //要管理的群名称
+  MYSELF: "仙女驻凡大使馆" //大号的昵称
+};
